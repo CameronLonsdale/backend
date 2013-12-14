c9d3fa649a57744035a6c37872c8ebfc6cc9ab5a
Benjamin Schaaf <Ben.Schaaf@Gmail.com>
2013-12-14 18:45:16 +1100

    Fixed Syntax in test suite

---
+++ Tests/test_all.py
@@ -0,0 +1,5 @@
+import test_auth
+
+if __name__ == "__main__":
+    target = "http://127.0.0.1:3000/v1"
+    test_auth.main(target)
--- Tests/test_auth.py
+++ Tests/test_auth.py
@@ -1,56 +1,57 @@
+from __future__ import print_function
 from Parse import Parse
 from requests import get as WWW
 
 def make_user(target):
     www = WWW(target + "/auth/register?username=test_test_test&password=lol&email=test@test.com&subscription=0&source=test")
-    print www.text
     out = Parse(www.text)
     
+    if out.error: print(www.text)
     return not out.error
 
 def login_user(target, password="lol"):
     www = WWW(target + "/auth/login?username=test_test_test&password=" + password)
-    print www.text
     out = Parse(www.text)
     
     if out.error:
+        print(www.text)
         return None
     return out.values
 
 def confirm_user(target, secure_code):
     www = WWW(target + "/auth/confirm?username=test_test_test&secure_code=" + secure_code)
-    print www.text
     out = Parse(www.text)
     
+    if out.error: print(www.text)
     return not out.error
 
 def change_password(target, ticket):
     www = WWW(target + "/auth/changepassword?username=test_test_test&ticket=" + ticket + "&new_password=lolies")
-    print www.text
     out = Parse(www.text)
     
+    if out.error: print(www.text)
     return not out.error
 
 def main(target):
     if not make_user(target):
-        print "Failed Making User"
+        raise Exception("Failed Making User")
     if make_user(target):
-        print "Failed User Duplication"
+        raise Exception("Failed User Duplication")
     
     values = login_user(target)
     if not values:
-        print "Failed Logging In"
+        raise Exception("Failed Logging In")
     
     if not confirm_user(target, values[1]):
-        print "Failed Confirming Secure Code"
+        raise Exception("Failed Confirming Secure Code")
     
     if not change_password(target, values[0]):
-        print "Failed Changing Password"
+        raise Exception("Failed Changing Password")
     
     if not login_user(target, password="lolies"):
-        print "Failed Logging in with Changed Password"
+        raise Exception("Failed Logging in with Changed Password")
     
-    print "Authentication Success!"
+    print("Authentication Success!")
 
 if __name__ == "__main__":
     main("http://127.0.0.1:3000/v1")
